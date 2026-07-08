5f1d5641f358af3dd26e3ad80d523b191cbc8705
hae506 <sunghae065@gmail.com>
2026-07-09 00:29:58 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-name.textContent=CONFIG.name;company.textContent=CONFIG.company;profileImg.src=CONFIG.profileImage;tag1.textContent=CONFIG.tags[0];tag2.textContent=CONFIG.tags[1];tag3.textContent=CONFIG.tags[2];call.href='tel:'+CONFIG.phone;gmail.href='https://mail.google.com/mail/?view=cm&fs=1&to='+encodeURIComponent(CONFIG.email);blog.href=CONFIG.blog;youtube.href=CONFIG.youtube;function saveContact(){const v=`BEGIN:VCARD
-VERSION:3.0
-FN:${CONFIG.name}
-ORG:${CONFIG.company}
-TEL:${CONFIG.phone}
-EMAIL:${CONFIG.email}
-END:VCARD`;const b=new Blob([v],{type:'text/vcard'});const a=document.createElement('a');a.href=URL.createObjectURL(b);a.download='contact.vcf';a.click();}
