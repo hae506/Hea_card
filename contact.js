f5fe764573c8f594f9f9972f3ce6acb17b2ab9d3
hae506 <sunghae065@gmail.com>
2026-07-10 10:11:37 +0900

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,7 +1,7 @@
 function saveMyContact() {
     console.log("버튼이 클릭되었습니다!"); // 이 줄 추가
     const name = "장학생";
-// contact.js
+
 function saveMyContact() {
     const name = "장학생";
     const phone = "010-0000-0000";
